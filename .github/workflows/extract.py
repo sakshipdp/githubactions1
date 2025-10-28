aabb32ac2b31651d96262560c52e5d460de85831
sakshipdp <sakshi.63001@gmail.com>
2025-10-28 19:00:27 +0530

    testing7

--- .github/workflows/extract.py
+++ .github/workflows/extract.py
@@ -1,1 +1,12 @@
+import pandas as pd 
+
 print("extract data from mysql database")
+
+data ={
+   "id" : [ 1, 2, 3],
+   "name" : ["A","B","C"],
+   "age" : [10,20,30]
+}
+
+df=pd.DataFrame(data)
+print(df)
